4bcdb3279a841f9645819d9ab1d55a6b1378e2b8
derrosilee <derrossilee@gmail.com>
2024-02-23 11:47:54 +0300

    added env dependency for env files

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
 
 func main() {
 	fmt.Println("Hello world")
+
+	godotenv.Load()
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the Environment variable")
+	}
+	fmt.Println("PORT", portString)
 }
