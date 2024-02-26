b2e556e7a72fe7e4944d45595968fb960e7f00f0
Lee Derrosi <derrossilee@gmail.com>
2024-02-26 07:22:39 +0300

    added cores vendor files

--- main.go
+++ main.go
@@ -2,19 +2,40 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 	"log"
+	"net/http"
 	"os"
 )
 
 func main() {
 	fmt.Println("Hello world")
 
-	godotenv.Load()
+	err := godotenv.Load()
+	if err != nil {
+		return
+	}
 
 	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("PORT is not found in the Environment variable")
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler)
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server running on Port %v", portString)
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
 	}
 	fmt.Println("PORT", portString)
 }
