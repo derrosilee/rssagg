1c9bc353ff88e5ddeec8cb9499088de699b48a5a
Lee Derrosi <derrossilee@gmail.com>
2024-03-02 15:27:21 +0300

    added user creation

--- handler_user.go
+++ handler_user.go
@@ -1,8 +1,35 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/derrosilee/rssagg/internal/database"
+	"github.com/google/uuid"
+	"net/http"
+	"time"
+)
 
 func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
-	respondWithJSON(w, 200, struct {
-	}{})
+	type parameters struct {
+		Name string `json:"name"`
+	}
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Error Parsing JSON: %v", err))
+		return
+	}
+
+	user, err := apiCfg.DB.CreateUser(r.Context(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      params.Name,
+	})
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldnt Create User: %v", err))
+		return
+	}
+	respondWithJSON(w, 200, user)
 }
