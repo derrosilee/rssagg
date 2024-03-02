b62f3c0ca69260493e40bcb9877e71ef559cea37
Lee Derrosi <derrossilee@gmail.com>
2024-03-02 15:05:27 +0300

    added user handler_user.go

--- main.go
+++ main.go
@@ -57,6 +57,7 @@ func main() {
 	v1Router := chi.NewRouter()
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/err", handleErr)
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
 	router.Mount("/v1", v1Router)
 
 	srv := &http.Server{
