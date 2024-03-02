73d62dfdc51df081c08e7b56ee2311d2731c90f2
Lee Derrosi <derrossilee@gmail.com>
2024-03-02 16:19:47 +0300

    added get user functionality to handlers

--- handler_user.go
+++ handler_user.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/derrosilee/rssagg/internal/auth"
 	"github.com/derrosilee/rssagg/internal/database"
 	"github.com/google/uuid"
 	"net/http"
@@ -31,9 +32,19 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 		respondWithError(w, 400, fmt.Sprintf("Couldnt Create User: %v", err))
 		return
 	}
-	respondWithJSON(w, 200, databaseUserToUser(user))
+	respondWithJSON(w, 201, databaseUserToUser(user))
 }
 
 func (apiCfg *apiConfig) handlerUserGetUser(w http.ResponseWriter, r *http.Request) {
-
+	apiKey, err := auth.GetAPIKey(r.Header)
+	if err != nil {
+		respondWithError(w, 403, fmt.Sprintf("Authentication error"))
+		return
+	}
+	user, err := apiCfg.DB.GetUserByAPIKey(r.Context(), apiKey)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldn't Retrieve User: %v", err))
+		return
+	}
+	respondWithJSON(w, 200, databaseUserToUser(user))
 }
