15484787af8441f3d247b7ced675df445341af99
Lee Derrosi <derrossilee@gmail.com>
2024-03-04 06:47:08 +0300

    added Feeds Endpoint

--- main.go
+++ main.go
@@ -59,6 +59,8 @@ func main() {
 	v1Router.Get("/err", handleErr)
 	v1Router.Post("/users", apiCfg.handlerCreateUser)
 	v1Router.Get("/users", apiCfg.middlewareAuth(apiCfg.handlerUserGetUser))
+	v1Router.Get("/feeds", apiCfg.middlewareAuth(apiCfg.handlerCreateFeed))
+
 	router.Mount("/v1", v1Router)
 
 	srv := &http.Server{
