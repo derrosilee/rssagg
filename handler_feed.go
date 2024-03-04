d9d283e982976560432b6b2ca15863c70310915f
Lee Derrosi <derrossilee@gmail.com>
2024-03-04 06:43:06 +0300

    added create feed functionality

---
+++ handler_feed.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/derrosilee/rssagg/internal/database"
+	"github.com/google/uuid"
+	"net/http"
+	"time"
+)
+
+func (apiCfg *apiConfig) handlerCreateFeed(w http.ResponseWriter, r *http.Request, user database.User) {
+	type parameters struct {
+		Name string `json:"name"`
+		Url  string `json:"url"`
+	}
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Error Parsing JSON: %v", err))
+		return
+	}
+
+	feed, err := apiCfg.DB.CreateFeed(r.Context(), database.CreateFeedParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      params.Name,
+		Url:       params.Url,
+		UserID:    user.ID,
+	})
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldnt Create Feed: %v", err))
+		return
+	}
+	respondWithJSON(w, 201, databaseFeedToFeed(feed))
+}
