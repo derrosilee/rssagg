37bb336efca2dcd2cde48775b62f3ba9f9c8462f
Lee Derrosi <derrossilee@gmail.com>
2024-03-05 07:15:58 +0300

    added feed_follows handler_feed_follows.go

---
+++ handler_feed_follows.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/derrosilee/rssagg/internal/database"
+	"github.com/google/uuid"
+	"net/http"
+	"time"
+)
+
+func (apiCfg *apiConfig) handlerCreateFeedFollow(w http.ResponseWriter, r *http.Request, user database.User) {
+	type parameters struct {
+		FeedID uuid.UUID `json:"feed_id"`
+	}
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Error Parsing JSON: %v", err))
+		return
+	}
+
+	feed, err := apiCfg.DB.CreateFeedFollow(r.Context(), database.CreateFeedFollowParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		UserID:    user.ID,
+		FeedID:    params.FeedID,
+	})
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldnt Create Feed Follow: %v", err))
+		return
+	}
+	respondWithJSON(w, 201, databaseFeedToFeed(feed))
+}
