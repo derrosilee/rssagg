038051e1e4f2d2d675ffecbcd88881104e8e552c
Lee Derrosi <derrossilee@gmail.com>
2024-03-08 07:16:42 +0300

    added feed_follows Delete follow query

--- handler_feed_follows.go
+++ handler_feed_follows.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/derrosilee/rssagg/internal/database"
+	"github.com/go-chi/chi"
 	"github.com/google/uuid"
 	"net/http"
 	"time"
@@ -42,4 +43,24 @@ func (apiCfg *apiConfig) handlerGetFeedFollows(w http.ResponseWriter, r *http.Re
 		return
 	}
 	respondWithJSON(w, 201, databaseFeedFollowsToFeedFollows(feedFollows))
+}
+
+func (apiCfg *apiConfig) handlerDeleteFeedFollow(w http.ResponseWriter, r *http.Request, user database.User) {
+	feedFollowIDStr := chi.URLParam(r, "feedFollowID")
+	feedFollowID, err := uuid.Parse(feedFollowIDStr)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldnt Parse feed follow ID: %v", err))
+		return
+	}
+
+	err = apiCfg.DB.DeleteFeedFollow(r.Context(), database.DeleteFeedFollowParams{
+		ID:     feedFollowID,
+		UserID: user.ID,
+	})
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldnt Delete feed follow : %v", err))
+		return
+	}
+
+	respondWithJSON(w, 200, struct{}{})
 }
